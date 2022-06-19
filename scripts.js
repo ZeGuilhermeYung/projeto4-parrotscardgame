672c35f753a95c51967d38897da14a492b58d6fc
ZeGuilhermeYung <jgryung@gmail.com>
2022-06-18 21:19:43 -0300

    função de embaralhar cartas

--- scripts.js
+++ scripts.js
@@ -1,12 +1,29 @@
-let numberOfCards = prompt("Com quantas cartas deseja jogar?\n(Escolha um nº entre 4 a 14)");
-checkNumber();
+let manyCards = prompt("Com quantas cartas deseja jogar?\n(Escolha um nº entre 4 a 14)");
+const cardList = ["card1", "card2", "card3", "card4", "card5", "card6", "card7"];
+const gameCardList = [];
 
-function checkNumber(numberOfCards) {
-    numberOfCards = Number(numberOfCards);
-    if ((numberOfCards >= 4) && (numberOfCards <= 14) && (numberOfCards % 2 == 0)) {
-        alert("deu certo!");
+checkNumber(manyCards);
+
+function checkNumber(num) {
+    num = Number(num);
+    if ((num >= 4) && (num <= 14) && (num % 2 == 0)) {
+        //alert("deu certo!");
+        startGame(num);
     } else {
-        numberOfCards = prompt("Com quantas cartas deseja jogar?\n(Escolha um nº entre 4 a 14)");
-        checkNumber(numberOfCards);
+        num = prompt("Com quantas cartas deseja jogar?\n(Escolha um nº entre 4 a 14)");
+        checkNumber(num);
     }
+}
+
+function startGame (numberOfCards) {
+    cardList.sort(scramble);
+    for (let i = 0; i < numberOfCards/2; i++) {
+        gameCardList.push(cardList[i]);
+        gameCardList.push(cardList[i]);
+    }
+    gameCardList.sort(scramble);
+    alert(gameCardList);
+}
+function scramble() { 
+	return Math.random() - 0.5; 
 }
